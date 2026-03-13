5e340e18dc87d7a36c65d6dd5effcbcf660d2a91
yakumouwu <1014242991@qq.com>
2026-03-13 10:08:17 +0800

    Remove local auto-commit scripts from main

--- auto_commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d D:\reverse
-powershell -NoProfile -ExecutionPolicy Bypass -File "D:\reverse\auto_commit.ps1"
-exit /b %errorlevel%
-
--- register_auto_commit_task.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-schtasks /Create /TN "MysekaiAutoCommit" /SC MINUTE /MO 10 /TR "\"D:\reverse\auto_commit.bat\"" /F
-echo Task created: MysekaiAutoCommit
-echo It will run every 10 minutes.
-echo You can also run D:\reverse\auto_commit.bat manually for a one-time sync.
-pause
